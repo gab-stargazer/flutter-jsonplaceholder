9cbf68e27e178d830ac05387135d7c729b24a1b1
LeleStacia <km8003296@gmail.com>
2023-07-07 02:29:50 +0700

    Initial Commit

---
+++ android/app/src/main/kotlin/com/lelestacia/json_placeholder_album/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.lelestacia.json_placeholder_album
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/MyApp.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'main.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Json Placeholder',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
